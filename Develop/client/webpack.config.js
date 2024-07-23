253e14424bca8262a0b38ecea2c71e695bbf8a7b
MathewG2099 <160433914+MathewG2099@users.noreply.github.com>
2024-07-22 20:41:37 -0400

    config

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -47,11 +47,29 @@ new WebpackPwaManifest({
     },
   ],
 }),
-],
+
   
 
     module: {
       rules: [
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader']
+        },
+        {
+          test: /\.m?js$/,
+          exlude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+              plugins: [
+                "@babel/plugin-proposal-object-rest-spread",
+                "@babel/transform-runtime",
+              ]
+            }
+          }
+        }
         
       ],
     },
